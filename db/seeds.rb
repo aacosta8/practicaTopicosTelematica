2a98102178b5af284e435f5ef6b1ea9b2464f7f9
Alex <aacosta8@eafit.edu.co>
2017-08-03 16:43:36 -0500

    Seeding development database with test user and test image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
 Publication.delete_all
-Publication.create!( id: 1, name: "Foto1", description:"foto de prueba 1", place: "Medelín", permission_pub: 0, user_id:1 )
-Publication.create!( id: 2, name: "Foto2", description:"foto de prueba 2", place: "Medelín", permission_pub: 1, user_id:1 )
-Publication.create!( id: 3, name: "Foto3", description:"foto de prueba 3", place: "Medelín", permission_pub: 2, user_id:1)
+User.delete_all
+user = User.new
+user.email = 'aacosta8@eafit.edu.co'
+user.password = '123456'
+user.password_confirmation = '123456'
+user.save!
+
+
+Publication.create!( id: 1, name: "Foto1", description:"foto de prueba 1", place: "Medelín", permission_pub: 0, user_id:1, photo: File.new("/home/alex/Imágenes/Wallpapers/beautiful_abstract-HD.jpg") )
+Publication.create!( id: 2, name: "Foto2", description:"foto de prueba 2", place: "Medelín", permission_pub: 1, user_id:1, photo: File.new("/home/alex/Imágenes/Wallpapers/beautiful_abstract-HD.jpg"))
+Publication.create!( id: 3, name: "Foto3", description:"foto de prueba 3", place: "Medelín", permission_pub: 2, user_id:1, photo: File.new("/home/alex/Imágenes/Wallpapers/beautiful_abstract-HD.jpg"))
