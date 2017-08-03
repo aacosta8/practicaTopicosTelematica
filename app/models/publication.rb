e6dd5664008daad1090ec799e7a946a787ae7e13
Alex <aacosta8@eafit.edu.co>
2017-08-03 00:20:29 -0500

    Permisos con scopes, cambios en el modelo

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,9 +1,9 @@
 class Publication < ApplicationRecord
 	enum permission_pub: [ :publico,  :compartido, :privado]
 
-	scope :publicos, ->{where(permission_pub: 0)}
-	scope :compartidos, ->{where('permission_pub != ?', 2)}
-	scope :privados, ->{where(permission_pub: 2)}
+	scope :publicos, ->{where('permission_pub < ?', 1)}
+	scope :compartidos, ->{where('permission_pub < ?', 2)}
+	scope :privados, ->{where('permission_pub < ?', 3)}
  	scope :recientes, ->{order("created_at DESC")}
 
 end
