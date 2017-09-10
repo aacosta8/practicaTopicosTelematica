4e824c21069a3a37fdfac13451af3bfe8f18072e
Julian <jarangol@eafit.edu.co>
2017-09-10 15:24:18 -0500

    Precompilando assets de nuevo para testear

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.cache_classes = false
+  config.assets.compile = false
+  config.cache_classes = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
